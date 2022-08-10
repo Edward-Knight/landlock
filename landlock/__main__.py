e49a844d683a63557d81abf5f0ce661a0625869b
Edward Knight <edw@rdknig.ht>
2022-08-10 14:56:29 -0600

    Draw the rest of the owl

--- landlock/__main__.py
+++ landlock/__main__.py
@@ -1,9 +1,18 @@
-from landlock.porcelain import landlock_abi_version
+from os.path import expanduser
+from landlock import FSAccess, Ruleset
+from landlock.plumbing import landlock_abi_version
 
 
 def main():
     abi = landlock_abi_version()
     print(f"Landlock LSM is enabled, ABI version {abi}")
+    rs = Ruleset()
+    rs.allow("/tmp", FSAccess.all())
+    rs.apply()
+    with open("/tmp/test", "w") as f:
+        f.write("test")
+    with open(expanduser("~/test"), "w") as f:
+        f.write("test")
 
 
 if __name__ == "__main__":
--- landlock/porcelain.py
+++ landlock/porcelain.py
@@ -1,5 +1,48 @@
-from landlock.plumbing import _create_ruleset, CREATE_RULESET_VERSION
+import ctypes
+import dataclasses
+import os
+
+from landlock import LandlockError
+from landlock.plumbing import (
+    PR_SET_NO_NEW_PRIVS,
+    FSAccess,
+    PathBeneathAttr,
+    RulesetAttr,
+    get_add_rule,
+    get_create_ruleset,
+    get_prctl,
+    get_restrict_self,
+)
 
 
-def landlock_abi_version() -> int:
-    return _create_ruleset(None, 0, CREATE_RULESET_VERSION)
+@dataclasses.dataclass(frozen=True)
+class Ruleset:
+    restrict_rules: FSAccess = FSAccess.all()
+
+    def __post_init__(self):
+        ruleset_attr = RulesetAttr(self.restrict_rules)
+        fd = get_create_ruleset()(
+            ctypes.byref(ruleset_attr),
+            ctypes.sizeof(ruleset_attr),
+            0,
+        )
+        object.__setattr__(self, "_fd", fd)
+
+    def allow(self, path, rules):
+        fd = os.open(path, flags=os.O_PATH)
+        try:
+            rule_attr = PathBeneathAttr(rules, fd)
+            get_add_rule()(self._fd, 1, ctypes.byref(rule_attr), 0)
+        finally:
+            os.close(fd)
+
+    def apply(self):
+        # restrict thread from gaining privileges
+        try:
+            prctl = get_prctl()
+        except Exception as e:
+            raise LandlockError("Cannot find prctl libc function") from e
+        prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)
+
+        # turn on Landlock restrictions
+        get_restrict_self()(self._fd, 0)
